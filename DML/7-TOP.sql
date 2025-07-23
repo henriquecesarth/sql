d623e8da86567bb72a87ce36f0d6838672d67f37
Henrique César <henriquecesarth22@gmail.com>
2025-07-23 16:28:34 -0300

    Create 7-TOP.sql

---
+++ DML/7-TOP.sql
@@ -0,0 +1,6 @@
+-- Traz apenas os 2 primeiros registros da tabela
+SELECT TOP 2
+    first_name,  -- Nome do cliente
+    country,     -- País do cliente
+    score        -- Pontuação do cliente
+FROM customers;
