d4c1c70db710ec27ff1195ed3d0ca13433ef92fd
Henrique César <henriquecesarth22@gmail.com>
2025-07-23 16:40:02 -0300

    Create 9-Static_Value.sql

---
+++ DML/9-Static_Value.sql
@@ -0,0 +1,5 @@
+-- Seleciona um valor fixo (string) 'Hello World!' e dá o nome de coluna "greeting"
+-- Para cada linha da tabela customers, essa mesma mensagem será repetida
+SELECT
+    'Hello World!' AS greeting
+FROM customers;
