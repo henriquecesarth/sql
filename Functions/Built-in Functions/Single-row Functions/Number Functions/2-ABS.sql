b7da5e1adc52522b70d69066226fda1c88e6d656
Henrique César <henriquecesarth22@gmail.com>
2025-07-29 11:18:30 -0300

    Create 2-ABS.sql

---
+++ Functions/Built-in Functions/Single-row Functions/Number Functions/2-ABS.sql
@@ -0,0 +1,7 @@
+SELECT 
+    -10 AS number,  -- Número original negativo
+    ABS(-10) AS absolute_value
+    -- A função ABS (abreviação de "absolute") retorna o valor absoluto do número,
+    -- ou seja, transforma um número negativo em positivo e mantém positivos inalterados.
+    -- Isso é útil em situações onde só o valor numérico importa, independentemente do sinal,
+    -- como em distâncias, magnitudes ou comparações matemáticas.
